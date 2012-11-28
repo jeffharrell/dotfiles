b65274361ec5c2b66fa01ef56d7c5691704ce358
Jeff Harrell <jeff@juxtadesign.com>
2012-11-27 22:26:56 -0800

    Removing .gitignore from the linked files

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ read -p "This may overwrite files in your home directory. Are you sure? (y/n) " 
 
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	for file in `ls -A | grep -v -e README.md -e setup.sh -e .git$`; do
+	for file in `ls -A | grep -v -e README.md -e setup.sh -e .git$ -e .gitignore`; do
 		ln -fs "$PWD/$file" "$HOME/$file";
 		echo "$PWD/$file -> $HOME/$file";
 	done
